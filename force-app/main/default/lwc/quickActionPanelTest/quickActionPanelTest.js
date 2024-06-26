1fe707ed59ff498da06d55021109fdd98d779ac3
heapallocation <mrjohnbartlett@gmail.com>
2024-06-26 21:05:34 +1200

    quick action panel test

---
+++ force-app/main/default/lwc/quickActionPanelTest/quickActionPanelTest.js
@@ -0,0 +1,16 @@
+// myModal.js
+import { LightningElement } from "lwc";
+
+export default class MyModal extends LightningElement {
+  handleOk() {
+    // Handle the 'OK' button click
+  }
+
+  handleCancel() {
+    // Handle the 'Cancel' button click
+  }
+
+  handleClose() {
+    // Handle the close event
+  }
+}
