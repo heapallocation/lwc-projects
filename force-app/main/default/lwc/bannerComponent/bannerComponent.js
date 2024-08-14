4adac2add3ce9ad7904c6d0bdfca2d6d9a31a8c8
heapallocation <mrjohnbartlett@gmail.com>
2024-08-14 20:56:48 +1200

    updated js file

--- force-app/main/default/lwc/bannerComponent/bannerComponent.js
+++ force-app/main/default/lwc/bannerComponent/bannerComponent.js
@@ -1,5 +1,11 @@
 import { LightningElement, api } from "lwc";
-
 export default class BannerComponent extends LightningElement {
+  @api headerText;
   @api message;
+  @api bannerColor;
+
+  renderedCallback() {
+    this.template.querySelector(".banner").style.backgroundColor =
+      this.bannerColor;
+  }
 }
