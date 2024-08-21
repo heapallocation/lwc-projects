45f5ccf241384d2d89def9bc649bd4cb78f5f5d4
heapallocation <mrjohnbartlett@gmail.com>
2024-08-21 20:27:46 +1200

    js body

--- force-app/main/default/lwc/taskUtility/taskUtility.js
+++ force-app/main/default/lwc/taskUtility/taskUtility.js
@@ -1,3 +1,48 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from "lwc";
+import { createRecord } from "lightning/uiRecordApi";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
-export default class TaskUtility extends LightningElement {}
+export default class CreateTask extends LightningElement {
+  @track whoId = "";
+  @track whatId = "";
+
+  handleWhoIdChange(event) {
+    this.whoId = event.target.value;
+  }
+
+  handleWhatIdChange(event) {
+    this.whatId = event.target.value;
+  }
+
+  handleCreateTask() {
+    const fields = {
+      Subject: "Follow up",
+      Status: "Not Started",
+      Priority: "Normal",
+      WhoId: this.whoId, // Polymorphic field for Contact or Lead
+      WhatId: this.whatId // Polymorphic field for Account or Opportunity
+    };
+
+    const taskRecordInput = { apiName: "Task", fields };
+
+    createRecord(taskRecordInput)
+      .then((task) => {
+        this.dispatchEvent(
+          new ShowToastEvent({
+            title: "Success",
+            message: "Task created successfully",
+            variant: "success"
+          })
+        );
+      })
+      .catch((error) => {
+        this.dispatchEvent(
+          new ShowToastEvent({
+            title: "Error creating task",
+            message: error.body.message,
+            variant: "error"
+          })
+        );
+      });
+  }
+}
